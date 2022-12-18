dda219cee921d233b91a839f068109d771b10492
srinivasthedeveloper <srinivasthedeveloper@gmail.com>
2022-12-18 14:52:27 +0530

    added scss and clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://srinivasthedeveloper.github.io',
-  base: '/astro-portfolio',
+  site: 'https://srinivasthedeveloper.github.io/astro-portfolio',
+  base: '',
   integrations: [react(), tailwind()]
 });
