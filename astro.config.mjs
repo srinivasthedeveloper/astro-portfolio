3dfb7d679671bfdb69ad0ec7e1ad93d7e43b1249
srinivasthedeveloper <srinivasthedeveloper@gmail.com>
2022-12-18 13:39:05 +0530

    added deploy.yml in github workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://srinivasthedeveloper.github.io',
+  base: '/astro-portfolio',
   integrations: [react(), tailwind()]
 });
